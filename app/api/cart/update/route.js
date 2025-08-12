f53f3fa5c0f3598ef316036b229600a5ae777426
sarim-alis <sarimslayerali786@gmail.com>
2025-08-12 09:20:36 +0500

    feat/Address section done

--- app/api/cart/update/route.js
+++ app/api/cart/update/route.js
@@ -1,6 +1,6 @@
 import connectDB from "@/config/db";
 import User from "@/models/User";
-import { getAuth } from "@clerk/nextjs/dist/types/server";
+import { getAuth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
 
@@ -15,7 +15,7 @@ export async function POST (request) {
       const user = await User.findById(userId)
 
       user.cartItems = cartData
-      user.save()
+      await user.save()
 
       return NextResponse.json({ success: true });
 
--- context/AppContext.jsx
+++ context/AppContext.jsx
@@ -61,7 +61,6 @@ export const AppContextProvider = (props) => {
 
       } catch (error) {
       toast.error(error.message)
-
       }
     }
 
@@ -75,7 +74,15 @@ export const AppContextProvider = (props) => {
             cartData[itemId] = 1;
         }
         setCartItems(cartData);
-        toast.success('Item added to cart 🛒')
+        if (user) {
+            try {
+                const token = await getToken()
+                await axios.post(`/api/cart/update`, {cartData}, { headers: { Authorization: `Bearer ${token}` }})
+                toast.success('Item added to cart 🛒')
+            } catch (error) {
+                toast.error(error.message)
+            }
+        }
     }
 
     const updateCartQuantity = async (itemId, quantity) => {
@@ -87,7 +94,15 @@ export const AppContextProvider = (props) => {
             cartData[itemId] = quantity;
         }
         setCartItems(cartData)
-
+        if (user) {
+            try {
+                const token = await getToken()
+                await axios.post(`/api/cart/update`, {cartData}, { headers: { Authorization: `Bearer ${token}` }})
+                toast.success('Cart updated 🛍️')
+            } catch (error) {
+                toast.error(error.message)
+            }
+        }
     }
 
     const getCartCount = () => {
