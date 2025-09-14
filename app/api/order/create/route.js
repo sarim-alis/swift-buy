ffaab51d4ec4cb2488307de7afc19ca5e830bd2c
sarim-alis <sarimslayerali786@gmail.com>
2025-09-15 02:02:10 +0500

    feat/Order created

--- app/api/order/create/route.js
+++ app/api/order/create/route.js
@@ -1,6 +1,8 @@
 import Product from "@/models/Product";
+import User from "@/models/User";
 import { getAuth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
+import { inngest } from "@/config/inngest";
 
 
 
@@ -20,8 +22,24 @@ export async function POST(request) {
             return acc + product.offerPrice * item.quantity;
         }, 0)
 
+        await inngest.send({
+            name: 'order/created',
+            data: {
+                userId,
+                address,
+                items,
+                amount: amount + Math.floor(amount * 0.02),
+                date: Date.now()
+            }
+        })
 
-        
+        // Clear user cart.
+        const user = await User.findById(userId)
+        user.cartItmes = {}
+        await user.save()
+
+        return NextResponse.json({ success: true, message: 'Order Placed' });
+
 } catch (error) {
     console.log(error);
     return NextResponse.json({ success: false, message: error.message });
