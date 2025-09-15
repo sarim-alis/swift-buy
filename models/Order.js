319ce563a610e7ed83704b882cd2855afd182e1a
sarim-alis <sarimslayerali786@gmail.com>
2025-09-15 06:55:07 +0500

    feats/Order get user

--- models/Order.js
+++ models/Order.js
@@ -1,18 +1,19 @@
 import mongoose from "mongoose";
 
-
 const orderSchema = new mongoose.Schema({
-    userId: { type: String, required: true, ref: 'user' },
-    items: [{
-        product: { type: String, required: true, ref: 'product' },
-        quantity: { type: Number, required: true }
-    }],
-    amount: { type: Number, required: true },
-    address: { type: String, ref: 'address', required: true },
-    status: { type: String, required: true, default: 'Order placed' },
-    date: { type: Number, required: true }
-})
+  userId: { type: String, required: true, ref: "User" },
+  items: [
+    {
+      product: { type: mongoose.Schema.Types.ObjectId, ref: "Product", required: true },
+      quantity: { type: Number, required: true },
+    },
+  ],
+  amount: { type: Number, required: true },
+  address: { type: mongoose.Schema.Types.ObjectId, ref: "Address", required: true },
+  status: { type: String, required: true, default: "Order placed" },
+  date: { type: Number, required: true },
+});
 
-const Order = mongoose.models.order || mongoose.model('order', orderSchema)
+const Order = mongoose.models.Order || mongoose.model("Order", orderSchema);
 
-export default Order
+export default Order;
