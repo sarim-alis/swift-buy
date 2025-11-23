8a983413203e2e12cc774171634fb05d016ba81e
sarim-alis <sarimslayerali786@gmail.com>
2025-11-24 00:52:21 +0500

    feat/Navbar setup

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,5 +1,6 @@
 "use client"
-import React from "react";
+import React, { useState } from "react";
+import { Search } from "lucide-react";
 import { assets} from "@/assets/assets";
 import Link from "next/link"
 import { useAppContext } from "@/context/AppContext";
@@ -10,15 +11,18 @@ const Navbar = () => {
 
   const { isSeller, router, user } = useAppContext();
   const {openSignIn} = useClerk();
+  const [search, setSearch] = useState("");
+
+  const handleSearch = (e) => {
+    e.preventDefault()
+    router.push(`/shop?search=${search}`)
+  }
 
   return (
     <nav className="flex items-center justify-between px-6 md:px-16 lg:px-32 py-3 border-b border-gray-300 text-gray-700">
-      <Image
-        className="cursor-pointer w-28 md:w-32"
-        onClick={() => router.push('/')}
-        src={assets.logo}
-        alt="logo"
-      />
+      <Link href="/" className="text-3xl md:text-4xl font-semibold text-slate-700 hover:opacity-80 transition">
+            <span className="text-[#ea580c]">shop</span>dock<span className="text-[#ea580c] text-4xl md:text-5xl leading-0">.</span>
+          </Link>
       <div className="flex items-center gap-4 lg:gap-8 max-md:hidden">
         <Link href="/" className="hover:text-gray-900 transition">
           Home
@@ -32,6 +36,10 @@ const Navbar = () => {
         <Link href="/" className="hover:text-gray-900 transition">
           Contact
         </Link>
+        <form onSubmit={handleSearch} className="hidden xl:flex items-center w-xs text-sm gap-2 bg-slate-100 px-4 py-3 rounded-full">
+          <Search size={18} className="text-slate-600" />
+          <input className="w-full bg-transparent outline-none placeholder-slate-600" type="text" placeholder="Search products" value={search} onChange={(e) => setSearch(e.target.value)} required />
+        </form>
 
         {isSeller && <button onClick={() => router.push('/seller')} className="text-xs border px-4 py-1.5 rounded-full">Seller Dashboard</button>}
 
